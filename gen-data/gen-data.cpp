f81321cf0571b7f68ec9e7a863eb253a36573cbb
ch0ndawg <choni0281@gmail.com>
2016-09-20 14:25:58 -0700

    Outputting generation data in JSON

--- gen-data/gen-data.cpp
+++ gen-data/gen-data.cpp
@@ -9,22 +9,28 @@
 
 using namespace std;
 
-int hash(int i, int j, int N)
+int matIndex(int i, int j, int N)
 {
     return N*j + i;
 }
 
 int main(int argc, char* argv[])
 {
+  double mean_increment = 1;
+  double increment_error = 0.1;
+  double gen_min = 0.0;
+  double gen_max = 10.0;
+
   unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
   std::default_random_engine generator (seed);
+
   if (argc < 3) {
     cerr << "USAGE: gen_rand numitems num_x num_y" << endl;
     return(EXIT_FAILURE);
   }
 
-  normal_distribution<double> nd;
-
+  normal_distribution<double> nd(mean_increment,increment_error);
+  uniform_real_distribution<double> ud(gen_min, gen_max);
   istringstream numitems(argv[1]);
   istringstream numx(argv[2]);
   istringstream numy(argv[3]);
@@ -34,15 +40,17 @@ int main(int argc, char* argv[])
   numx >> N;
   numy >> M;
 
-  vector<double>(N*M);
+  vector<double> nodes(N*M);
 
-  for (iter=0; iter < nIters; iter++) {
-
+  for (int iter=0; iter < nIters; iter++) {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        double increment = nd(generator);
+        nodes[matIndex(i,j,N)] += increment;
+        std::cout << "{ \"x\" :" << i << ", \"y\":" << j << ", \"timestamp\" : " << nodes[matIndex(i,j,N)]
+         << ", \"gen\": "<< ud(generator) << endl;
+      }
+    }
   }
-  // USAGE: gen_rand numitems num_x num_y
-  for (int i=0; i< nIters; i++) v.push_back(nd(generator));
-
-  double avg = accumulate(v.begin(),v.end(), 0.0, [](double x, double y) { return x+y;});
-  cout << avg /nIters << endl;
   return 0;
 }
