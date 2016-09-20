a29ddd210900481dc5352d6ae070577ef33a2035
ch0ndawg <choni0281@gmail.com>
2016-09-20 14:11:40 -0700

    fleshing out the random generation

--- gen-data/gen-data.cpp
+++ gen-data/gen-data.cpp
@@ -2,24 +2,47 @@
 #include <cmath>
 #include <chrono>
 #include <iostream>
+#include <sstream>
 #include <numeric>
 #include <vector>
 #include <random>
 
 using namespace std;
 
-int main()
+int hash(int i, int j, int N)
+{
+    return N*j + i;
+}
+
+int main(int argc, char* argv[])
 {
   unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
   std::default_random_engine generator (seed);
+  if (argc < 3) {
+    cerr << "USAGE: gen_rand numitems num_x num_y" << endl;
+    return(EXIT_FAILURE);
+  }
 
-  constexpr int TOTAL = 100000000;
   normal_distribution<double> nd;
-  vector<double> v;
-  v.reserve(TOTAL);
-  for (int i=0; i< TOTAL; i++) v.push_back(nd(generator));
+
+  istringstream numitems(argv[1]);
+  istringstream numx(argv[2]);
+  istringstream numy(argv[3]);
+
+  int nIters, N, M;
+  numitems >> nIters;
+  numx >> N;
+  numy >> M;
+
+  vector<double>(N*M);
+
+  for (iter=0; iter < nIters; iter++) {
+
+  }
+  // USAGE: gen_rand numitems num_x num_y
+  for (int i=0; i< nIters; i++) v.push_back(nd(generator));
 
   double avg = accumulate(v.begin(),v.end(), 0.0, [](double x, double y) { return x+y;});
-  cout << avg /TOTAL << endl;
+  cout << avg /nIters << endl;
   return 0;
 }
