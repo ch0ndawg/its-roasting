66006c52420a2b64de8af92b2fc03af4927652f5
ch0ndawg <choni0281@gmail.com>
2016-09-25 23:07:00 -0700

    missing a backslash

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-spark-submit \
+${SPARK_HOME}/bin/spark-submit \
 --master spark://ip-172-31-0-74:6066 \
---deploy-mode cluster
+--deploy-mode cluster \
 --supervise \
 --executor-cores 6 \
 /home/ubuntu/its-roasting/target/scala-2.10/it-s-roasting-assembly.jar 
