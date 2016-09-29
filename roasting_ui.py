1991a6d7fc0de08e2fdb40ad4857186a5072667f
ch0ndawg <choni0281@gmail.com>
2016-09-29 05:47:56 +0000

    Decently working un-animated and embedded temp distribution

--- roasting_ui.py
+++ roasting_ui.py
@@ -3,7 +3,6 @@ from flask import render_template
 from cassandra.cluster import Cluster
 from bokeh.io import curdoc
 from bokeh.models import ColumnDataSource, Div, Column
-from bokeh.plotting import figure, output_file
 from bokeh.plotting import figure
 from bokeh.resources import CDN
 from bokeh.embed import components
@@ -27,18 +26,20 @@ def heatgen(timestep):
     yvals = [row_list[j].y_coord for j in range(len(row_list))];
     xvals = [row_list[j].x_coord for j in range(len(row_list))];
     zvals = [row_list[j].temp for j in range(len(row_list))];
-    plotInfo = dict(x=xvals,y=yvals,z=zvals)
+    maxZ = 2*max(zvals)
 
-
-    colors = [
-     "#%02x%02x%02x" % (int(256*u),0,0) for u in zvals
+    heatmap = [
+     "#%02x%02x%02x" % (int(255.9*u),0,0) for u in zvals
     ]
-    output_file("test_heat.html", title="color_scatter.py example", mode="cdn")
+
+    plotInfo = dict(x=xvals,y=yvals,radius=[zval/maxZ for zval in zvals],colors=heatmap)
+    
+    dataSource = ColumnDataSource(plotInfo)
     TOOLS="resize,crosshair,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select"
 
     p = figure(tools=TOOLS, x_range=(-10,10), y_range=(-10,10))
-    p.circle(xvals,yvals,radius=0.5,fill_color=colors,fill_alpha=0.6,line_color=None)
-
+    # p.circle(xvals,yvals,radius=0.5,fill_color=colors,fill_alpha=0.6,line_color=None)
+    p.circle('x', 'y', source=dataSource, radius='radius', color='colors',alpha=0.75)
     script,div = components(p)
     return render_template('roasting.html', script=script, div=div)
 
