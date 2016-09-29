5eea019112f493513521e0cb8b3b58db369803e3
ch0ndawg <choni0281@gmail.com>
2016-09-28 21:10:39 -0700

    More bokeh integration, better embed

--- roasting_ui.py
+++ roasting_ui.py
@@ -5,7 +5,7 @@ from bokeh.models import ColumnDataSource, Div, Column
 from bokeh.plotting import figure, output_file
 from bokeh.plotting import figure
 from bokeh.resources import CDN
-from bokeh.embed import file_html
+from bokeh.embed import components
 from bokeh.util.string import encode_utf8
 
 import numpy as np
@@ -20,25 +20,27 @@ def hello():
 
 @app.route("/heatgen/<int:timestep>")
 def heatgen(timestep):
-    res = session.execute("select * from temps where time = %d" % timestep)
-    row_list = list(res)
+    for j=range(timestep,100):
+        res = session.execute("select * from temps where time = %d" % timestep)
+        row_list = list(res)
 
-    yvals = [row_list[j].y_coord for j in range(len(row_list))];
-    xvals = [row_list[j].x_coord for j in range(len(row_list))];
-    zvals = [row_list[j].temp for j in range(len(row_list))];
-    plotInfo = dict(x=xvals,y=yvals,z=zvals)
+        yvals = [row_list[j].y_coord for j in range(len(row_list))];
+        xvals = [row_list[j].x_coord for j in range(len(row_list))];
+        zvals = [row_list[j].temp for j in range(len(row_list))];
+        plotInfo = dict(x=xvals,y=yvals,z=zvals)
 
 
-    colors = [
-     "#%02x%02x%02x" % (int(256*u),0,0) for u in zvals
-    ]
-    output_file("test_heat.html", title="color_scatter.py example", mode="cdn")
-    TOOLS="resize,crosshair,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select"
+        colors = [
+         "#%02x%02x%02x" % (int(256*u),0,0) for u in zvals
+        ]
+        output_file("test_heat.html", title="color_scatter.py example", mode="cdn")
+        TOOLS="resize,crosshair,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select"
 
-    p = figure(tools=TOOLS, x_range=(-10,10), y_range=(-10,10))
-    p.circle(xvals,yvals,radius=0.5,fill_color=colors,fill_alpha=0.6,line_color=None)
+        p = figure(tools=TOOLS, x_range=(-10,10), y_range=(-10,10))
+        p.circle(xvals,yvals,radius=0.5,fill_color=colors,fill_alpha=0.6,line_color=None)
 
-    return encode_utf8(file_html(p, CDN,"Temperature at Time Step %d" % timestep))
+        script,div = components(plot)
+        return render_template('roasting.html', script=script, div=div)
 
 # @count()
 # def update(t):
