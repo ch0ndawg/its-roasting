44b9c9843a1f6a9c5ced754ac771fb0b4b10824c
ch0ndawg <choni0281@gmail.com>
2016-10-10 19:37:04 +0000

    backup

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 ${SPARK_HOME}/bin/spark-submit \
---master spark://ip-172-31-0-74:6066 \
---deploy-mode cluster \
+--master spark://ip-172-31-0-77:6066 \
 --supervise \
 --executor-cores 6 \
-/home/ubuntu/its-roasting/target/scala-2.10/it-s-roasting-assembly.jar 
+/home/ubuntu/its-roasting/target/scala-2.10/it-s-roasting-assembly-0.1.jar 40 5000 6 4 0.01
