e976a85cbd01c182a94708797ee475c8315ed6a4
ch0ndawg <choni0281@gmail.com>
2016-10-11 20:02:53 -0700

    testing the extra extra slow version

--- heat-gen-processor/src/com/nestedtori/heatgen/HeatGenProducer.java
+++ heat-gen-processor/src/com/nestedtori/heatgen/HeatGenProducer.java
@@ -73,10 +73,10 @@ public class HeatGenProducer implements Runnable {
 						long timestamp = Math.round(increment) + iter;
 						producer.send(new ProducerRecord<>("heatgen-input", new GridLocation(i,j),
 								new TimeTempTuple(timestamp,dtf)));
+						Thread.sleep(timeUnit); // pseudo batch in 100ms units
 						
 					}
 				}
-				Thread.sleep(timeUnit*4); // pseudo batch in 100ms units
 			}
 		} catch (InterruptedException ie) { }
 		finally { producer.close(); }
