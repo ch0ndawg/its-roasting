97771ab2c83b342ec228aad04633a38fc4819357
ch0ndawg <choni0281@gmail.com>
2017-04-21 02:15:17 +0000

    back to 5000 timesteps

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ ${SPARK_HOME}/bin/spark-submit \
 --master spark://ip-172-31-44-232:7077 \
 --supervise \
 --executor-cores 6 \
-/home/ubuntu/its-roasting/target/scala-2.11/it-s-roasting-assembly-0.1.jar 40 400 6 4 0.01
+/home/ubuntu/its-roasting/target/scala-2.11/it-s-roasting-assembly-0.1.jar 40 5000 6 4 0.01
